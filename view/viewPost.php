b1b3b093802f522c84f2e52ea991c205165a60b0
Bea Madjus <bea.madjus@gmail.com>
2018-05-25 06:07:00 +0800

    View post UI updated
    -added search in navbar

--- view/viewPost.php
+++ view/viewPost.php
@@ -1,5 +1,5 @@
 <?php
-    
+
 
     require '../vendor/autoload.php';
 
@@ -28,173 +28,187 @@
 
 
 
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+	<html>
+	  <head>
+	    <meta charset="utf-8" />
+	    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <title>View Post | Bloog</title>
+	    <title>View Post | Bloog</title>
 
-    <link rel="stylesheet" href="https://i.icomoon.io/public/temp/4045dc6036/UntitledProject/style.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="../assets/css/main.css" />
-  </head>
-<body>
+	    <link rel="stylesheet" href="https://i.icomoon.io/public/temp/a9a70307e1/UntitledProject/style.css">
+	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	    <link rel="stylesheet" href="../assets/css/main.css" />
+	  </head>
+	<body>
 
 
-	<div>
-    <!-- Header -->
-      <header id="header">
-        <h1><a href="#">Bloog</a></h1>
-        <nav class="links">
-          <ul>
-            <li><a href="../dashboard.php"><span class="icon-home2"></span>Home</a></li>
-            <li><a href="`../view/createPost.php"><span class="icon-pencil"></span>Write Post</a></li>
-          </ul>
-        </nav>
-        <nav class="main">
-          <ul>
-            <li class="search">
-                <input type="text" name="search" placeholder="Search" />
-            </li>
-            <li class="dropdown">
-                <a href="../profile.php" class="account" >
-                <img src="images/avatar.jpg" class="profile-circle"/>
-                </a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-  </div>
+		<div>
+	    <!-- Header -->
+	      <header id="header">
+	        <h1><a href="#">Bloog</a></h1>
+	        <nav class="links">
+	          <ul>
+	            <li><a href="../dashboard.php"><span class="icon-home2"></span>Home</a></li>
+	            <li><a href="`../view/createPost.php"><span class="icon-pencil"></span>Write Post</a></li>
+	          </ul>
+	        </nav>
 
 
 
-  <div class="container col-md-7">
+	            <!-- <li class="dropdown">
+	                <a href="../profile.php" class="account" >
+	                <img src="https://images.pexels.com/photos/925682/pexels-photo-925682.png?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" class="profile-circle"/>
+	                </a>
+	            </li> -->
 
-    <div class="image-container">
-      <img src="https://images.pexels.com/photos/925682/pexels-photo-925682.png?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260">
-    </div>
-    <div class="article-panel col-md-11">
-      <div class="article-title">
-        <h1><?php echo $post['title']?></h1>
-      </div>
-      <div class="about-author">
-        <a href="#" class="author-img"><img src="https://images.pexels.com/photos/324658/pexels-photo-324658.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" /></a>
-        <div class="name-dateposted">
-          <h6 class="author-name"><?php echo $post['username']?></h6>
-          <small class="date-posted"><?php echo $post['timestamp'] ?> </small>
-        </div>
-        <!-- <div class="follow-btn pull-right">
-          <button class="secondary-btn" type="submit" value="submit">Follow</button>
-        </div> -->
-      </div>
+	        <nav class="main search">
+	          <form class="form-search">
+	              <div class="input-append">
+	                  <input type="text" class="span2" placeholder="Search...">
+	                  <button type="submit" class="">Search</button>
 
-      <div class="col-md-11 article-content">
-        <p><?php echo file_get_contents($post['file']);?></p>
-      </div>
+	              </div>
+	          </form>
+	        </nav>
 
-      <hr style="width: 20%; margin-top: 5%!important; margin: 0 auto;"/>
-
-      <div class="comment-section col-md-10" style="margin: 0 auto; margin-top:10%;">
-        <label for="comment-area" class="article-title">Comment Section</label>
-        
-        <form method="POST" action="../controller/dashboardController.php">
-          <input type="hidden" name="functionCall" value="comment">
-          <input type="hidden" name="postID" value="<?php echo $post['_id']?>" >
-
-          <textarea name="comment-area" id="comment" style="height:200px;" rows="5" cols="10"></textarea>
-          <button class="pull-right publish-article" type="submit" value="submit">Post Comment</button>
-        </form>
-      </div>
-
-      <div class="posted-comments">
-        <div>
-
-          <div class="panel panel-default col-md-10" style="margin: 0 auto;">
-                <div class="panel-body">
-                    <ul class="media-list">
-
-
-                      <?php 
-                        if($comments == NULL)
-                        {
-                          echo "NO COMMENTS YET";
-                        }
-                        else
-                        {
-                          foreach($comments as $comment)
-                          {
-
-                      ?>
-
-
-                        <li class="media">
-                            <div class="media-left">
-                                <img src="http://placehold.it/60x60" class="img-circle">
-                            </div>
-                            <div class="media-body">
-                                <h4 class="media-heading">
-                                    <?php echo $comment['username'] ?>
-                                    <br>
-                                    <small>
-                                        commented on <a href="#"><?php echo $post['title'] ?></a>
-                                    </small>
-                                </h4>
-                                <p>
-                                    <?php echo $comment['comment'] ?>
-                                </p>
-                            </div>
-                        </li>
-
-                      <?php
-                          }
-                        }
-                      ?>
-
-<!--                         <li class="media">
-                            <div class="media-left">
-                                <img src="http://placehold.it/60x60" class="img-circle">
-                            </div>
-                            <div class="media-body">
-                                <h4 class="media-heading">
-                                    Aenean Consect
-                                    <br>
-                                    <small>
-                                        commented on <a href="#">Post Title</a>
-                                    </small>
-                                </h4>
-                                <p>
-                                    Curabitur vel malesuada tortor, sit amet ultricies mauris. Aenean consectetur ultricies luctus.
-                                </p>
-                            </div>
-                        </li>
-
-                        <li class="media">
-                            <div class="media-left">
-                               <img src="http://placehold.it/60x60" class="img-circle">
-                            </div>
-                            <div class="media-body">
-                                <h4 class="media-heading">
-                                    Praesent Tinci
-                                    <br>
-
-                                </h4>
-                                <p>
-                                    Sed convallis dignissim magna et dignissim. Praesent tincidunt sapien eu gravida dignissim.
-                                </p>
-                            </div>
-                        </li> -->
-                    </ul>
-                </div>
-            </div>
-
-        </div>
-      </div>
-
-    </div>
-  </div>
+	      </header>
+	  </div>
 
 
 
-</body>
-</html>
+	  <div class="container col-md-8">
+
+	    <div class="image-container">
+	      <img src="https://images.pexels.com/photos/925682/pexels-photo-925682.png?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260">
+	    </div>
+
+	    <div class="article-panel col-md-11" style="">
+
+	      <div class="about-author col-md-12" style="">
+	        <div style="" class="call-div col-md-11">
+	          <a href="#" class="author-img" style=""><img src="https://images.pexels.com/photos/324658/pexels-photo-324658.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="" /></a>
+	          <div class="name-dateposted" style="">
+	            <h6 class="author-name" style=""><?php echo $post['username']?></h6>
+	            <p class="date-posted" style=""><?php echo $post['timestamp'] ?> </p>
+	          </div>
+	        </div>
+	        <!-- <div class="follow-btn pull-right">
+	          <button class="secondary-btn" type="submit" value="submit">Follow</button>
+	        </div> -->
+
+	      <div class="article-title col-md-11" style="">
+	        <h1 style=""><?php echo $post['title']?></h1>
+	      </div>
+
+
+	      <div class="col-md-11 article-content">
+	        <p><?php echo file_get_contents($post['file']);?></p>
+	      </div>
+
+	      <hr style=""/>
+
+
+
+	    </div>
+	  </div>
+
+	  <div class="comment-section col-md-9" style="">
+	    <label for="comment-area" class="article-title" style="">Comment Section</label>
+
+	    <form method="POST" action="../controller/dashboardController.php">
+	      <input type="hidden" name="functionCall" value="comment">
+	      <input type="hidden" name="postID" value="<?php echo $post['_id']?>" >
+
+	      <textarea class="comment-box" name="comment-area" id="comment" style="" rows="5" cols="10" ;></textarea>
+	      <button class="pull-right publish-article" type="submit" value="submit" style="margin-top:2%;">Post Comment</button>
+	    </form>
+	  </div>
+
+	  <div class="posted-comments" style="">
+	    <div>
+
+	      <div class="panel panel-default col-md-9" style="">
+	            <div class="panel-body" style="">
+	                <ul class="media-list">
+
+
+	                  <?php
+	                    if($comments == NULL)
+	                    {
+	                      echo "NO COMMENTS YET";
+	                    }
+	                    else
+	                    {
+	                      foreach($comments as $comment)
+	                      {
+
+	                  ?>
+
+
+	                    <li class="media">
+	                        <div class="media-left">
+	                            <img style="margin-top:30%;" src="https://images.pexels.com/photos/1108249/pexels-photo-1108249.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" class="img-circle">
+	                        </div>
+	                        <div class="media-body">
+	                            <h4 class="media-heading" style="">
+	                                <?php echo $comment['username'] ?>
+	                                <br>
+	                                <p class="ewan">
+	                                    <a href="#"><?php echo $post['title'] ?></a>
+	                                </p>
+	                            </h4>
+	                            <p class="kumento" style="">
+	                              <?php echo $comment['comment'] ?>
+	                            </p>
+	                        </div>
+	                    </li>
+
+	                  <?php
+	                      }
+	                    }
+	                  ?>
+
+	<!--                         <li class="media">
+	                        <div class="media-left">
+	                            <img src="http://placehold.it/60x60" class="img-circle">
+	                        </div>
+	                        <div class="media-body">
+	                            <h4 class="media-heading">
+	                                Aenean Consect
+	                                <br>
+	                                <small>
+	                                    commented on <a href="#">Post Title</a>
+	                                </small>
+	                            </h4>
+	                            <p>
+	                                Curabitur vel malesuada tortor, sit amet ultricies mauris. Aenean consectetur ultricies luctus.
+	                            </p>
+	                        </div>
+	                    </li>
+
+	                    <li class="media">
+	                        <div class="media-left">
+	                           <img src="http://placehold.it/60x60" class="img-circle">
+	                        </div>
+	                        <div class="media-body">
+	                            <h4 class="media-heading">
+	                                Praesent Tinci
+	                                <br>
+
+	                            </h4>
+	                            <p>
+	                                Sed convallis dignissim magna et dignissim. Praesent tincidunt sapien eu gravida dignissim.
+	                            </p>
+	                        </div>
+	                    </li> -->
+	                </ul>
+	            </div>
+	        </div>
+
+	    </div>
+	  </div>
+
+
+
+	</body>
+	</html>
